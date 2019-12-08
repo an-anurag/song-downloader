5edf83116400077a5deb5c29ca2dfce56bc47f63
appyens <an.anurag@msn.com>
2019-12-09 02:00:53 +0530

    improved design

--- downloader.py
+++ downloader.py
@@ -10,81 +10,79 @@ from config import user_agents
 class SongDownloader:
 
     def __init__(self):
-        self.url = ""
+        self.records = open('album_records.txt', 'a+')
+        self.download_list = open('download_list.txt', 'r+')
 
-    def get_response(self, url):
-        randomint = random.randint(0, 7)
-        the_page = requests.get(url, headers={'User-Agent': user_agents[randomint]})
+    @staticmethod
+    def get_response(url):
+        choice = random.randint(0, 7)
+        the_page = requests.get(url, headers={'User-Agent': user_agents[choice]})
         return the_page
 
-    def make_soup(self, page):
+    @staticmethod
+    def make_soup(page):
         return Soup(page.text, 'html.parser')
 
-    def get_album_name(self, soup):
-        name = soup.find('h1', {'class': 'post-title'}).text
-        name = name[:-11]
+    @staticmethod
+    def get_album_name(soup):
+        name = soup.find('h1', {'class': 'post-title'}).text[:-11]
         return name
 
-    def make_dir(self, path, name):
-        return os.makedirs(path + str('/' + name), exist_ok=True)
-
-    def get_album_link(self, soup):
+    @staticmethod
+    def get_album_link(soup):
         live_album_urls = []
         for li in soup.find("section", {'class': 'primary'}).find_all('article'):
             anchor = li.find('a', href=True)['href']
-            name = li.find('a', href=True)
-            album_name = name.text[:-11]
             live_album_urls.append(anchor)
         return live_album_urls
 
-    def write_dl_list(self, dl_list):
-        with open('download_list.txt', 'a') as dl_file:
-            for link in dl_list:
-                dl_file.write(f"{link}\n")
-
-    def read_dl_list(self):
-        return [x for x in open('download_list.txt', 'r')]
-
-    def add_to_records(self, album, flag=True):
-        records = open('album_records.txt', 'a')
+    def add_to_records(self, album, flag):
         if flag:
-            records.write(f"{album}")
-            records.close()
+            self.records.write(f"{album}\n")
+            self.records.close()
 
-    def download_album(self):
-        dl_list = self.read_dl_list()
-        records = open('album_records.txt', 'r')
-        for url in dl_list:
-            is_written = False
-            resp = sd.get_response(url[:-1])
-            soup = sd.make_soup(resp)
-            for a in soup.find_all('tr')[1:]:
-                td = a.find_all('td')[1:]
-                bit320 = str(td[1].a['href'])
-                extension = bit320.split('.')[-1:][0]
-                file_name = bit320.split('/')[-1:][0]
-                title_search = re.findall('[A-Z]+[a-z]+', file_name)
-                title_extract = [x for x in title_search if x != "Downloadming" and x != "Kbps"]
-                title = ' '.join(title_extract)
-                song = sd.get_response(bit320)
-                print("Downloading... {}".format(title))
-                album_name = sd.get_album_name(soup)
-                path = os.path.dirname(os.path.abspath(__file__)) + '/' + album_name
-                os.makedirs(path, exist_ok=True)
+    def download_album(self, link):
+        resp = self.get_response(link)
+        soup = self.make_soup(resp)
+        album_name = self.get_album_name(soup)
+        print("ALBUM: %s" % album_name)
+        for a in soup.find_all('tr')[1:]:
+            td = a.find_all('td')[1:]
+            bit320 = str(td[1].a['href'])
+            file_name = bit320.split('/')[-1:][0]
+            extension = bit320.split('.')[-1:][0]
+            title_search = re.findall('[A-Z]+[a-z]+', file_name)
+            title = ' '.join(title_search[:-2])
+            path = os.path.dirname(os.path.abspath(__file__)) + '/' + album_name
+            os.makedirs(path, exist_ok=True)
+            if extension not in ['zip', 'Zip', 'ZIP']:
+                print("\t\tDownloading... {}".format(title))
+                song = self.get_response(bit320)
                 mp3 = open(path + '/' + title + '.' + extension, 'wb')
                 mp3.write(song.content)
-            is_written = True
-            self.add_to_records(url, is_written)
+        is_written = True
+        self.add_to_records(link, is_written)
 
 
-if __name__ == '__main__':
+def main():
     sd = SongDownloader()
-    url = "https://downloadming.cool/category/bollywood-mp3"
+    url = "https://downloadming2.com/category/bollywood-mp3"
     resp = sd.get_response(url)
     soup = sd.make_soup(resp)
     dl_list = sd.get_album_link(soup)
-    sd.write_dl_list(dl_list)
-    sd.download_album()
-    # TODO: handle repeated downloading of same album
-    # TODO: clear completed download list
-    # TODO: if zip album found download it and extract it dont download the tracks
+    content = sd.download_list.read().split('\n')
+    for item in dl_list:
+        if item not in content:
+            print(f"Adding {item} to download queue")
+            sd.download_list.write(f"{item}\n")
+    print("Download queue ready")
+    record_content = sd.records.read().split('\n')
+    for item in content:
+        if item not in record_content:
+            sd.download_album(item)
+        else:
+            print(f"{item} already downloaded")
+
+
+if __name__ == '__main__':
+    main()
